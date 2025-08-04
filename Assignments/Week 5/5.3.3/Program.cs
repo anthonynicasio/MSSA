f124c0c538686d9df4e8940aa1fc2836162943be
Anthony Nicasio <anthonydnicasio@gmail.com>
2025-08-04 18:37:17 -0400

    uploaded assignments

---
+++ Assignments/Week 5/5.3.3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Numerics;
+
+namespace _5._3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of the array");
+            if (!uint.TryParse(Console.ReadLine(), out uint arrayLen))
+            {
+                Console.WriteLine("Enter a valid positive integer.");
+            }
+
+            int[] userArray = new int[arrayLen];
+            int input;
+            Dictionary<int, int> intFrequency = new Dictionary<int, int>();
+
+            for (int i = 0; i < arrayLen; i++)
+            {
+                Console.Write($"Element - {i}: ");
+                while (!int.TryParse(Console.ReadLine(), out input))
+                {
+                    Console.WriteLine("Enter a valid integer.");
+                    Console.Write($"element - {i}: ");
+                }
+
+                if (intFrequency.ContainsKey(input))
+                {
+                    intFrequency[input]++;
+                }
+                else
+                {
+                    intFrequency[input] = 1;
+                }
+
+            }
+
+            bool hasMultiple = false;
+            foreach (var pair in intFrequency)
+            {
+                if (pair.Value == 1)
+                {
+                    Console.WriteLine($"{pair.Key} appears {pair.Value} time.");
+                    hasMultiple = false;
+                }
+                else
+                {
+                    Console.WriteLine($"{pair.Key} appears {pair.Value} times.");
+                    hasMultiple = true;
+                }
+            }
+            Console.WriteLine(hasMultiple);
+
+
+        }
+    }
+}
