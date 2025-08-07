3d8ab4ae62c97a32fe5ba8753da9f3a63e949c34
Anthony Nicasio <anthonydnicasio@gmail.com>
2025-08-06 20:28:31 -0400

    Corrected folder locations

---
+++ Assignments/Week 3/Challenge Labs/SumStringDigits/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SumStringDigits
+{
+    internal class Program
+    {
+        static int SumDigitsInString(string input)
+        {
+            int result = 0;
+            foreach (char c in input)
+            {
+                if (char.IsDigit(c))
+                {
+                    result += int.Parse($"{c}");
+                }
+
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your string:");
+            string? userInput = Console.ReadLine();
+            Console.WriteLine(SumDigitsInString(userInput));
+        }
+    }
+}
+
